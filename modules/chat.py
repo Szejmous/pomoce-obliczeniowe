4c8db0289a14198f82da79dd11f9fe343c84c613
Szejmous <arekstachyra97@gmail.com>
2025-03-17 21:51:44 +0100

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -6,7 +6,6 @@ import json
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-# Lista aktywnych połączeń WebSocket (z nazwą i kolorem użytkownika)
 active_connections = []
 
 @router.get("/", response_class=HTMLResponse)
@@ -17,26 +16,22 @@ async def chat(request: Request):
 async def websocket_chat(websocket: WebSocket):
     await websocket.accept()
     try:
-        # Odbierz pierwszą wiadomość z nazwą i kolorem użytkownika
         data = await websocket.receive_text()
         client_data = json.loads(data)
         client_name = client_data.get("user", "Anonim")
-        client_color = client_data.get("color", "#000000")  # Domyślny kolor: czarny
+        client_color = client_data.get("color", "#000000")
 
-        # Dodaj połączenie z nazwą i kolorem
         connection_info = {"websocket": websocket, "name": client_name, "color": client_color}
         active_connections.append(connection_info)
 
-        # Powiadom o dołączeniu użytkownika
         join_message = {"user": "System", "message": f"{client_name} dołączył do chatu", "color": "#000000"}
         await broadcast(json.dumps(join_message))
 
         while True:
-            # Odbieraj kolejne wiadomości
             data = await websocket.receive_text()
             message_data = json.loads(data)
             message_data["user"] = client_name
-            message_data["color"] = client_color  # Dodaj kolor do wiadomości
+            message_data["color"] = client_color
             await broadcast(json.dumps(message_data))
     except WebSocketDisconnect:
         active_connections[:] = [conn for conn in active_connections if conn["websocket"] != websocket]
@@ -47,6 +42,5 @@ async def websocket_chat(websocket: WebSocket):
         active_connections[:] = [conn for conn in active_connections if conn["websocket"] != websocket]
 
 async def broadcast(message: str):
-    # Wyślij wiadomość do wszystkich aktywnych połączeń
     for connection in active_connections:
         await connection["websocket"].send_text(message)
